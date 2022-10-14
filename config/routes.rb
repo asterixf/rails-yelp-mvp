3a3b47a3e454b771acbf6de4e9863ca21c9a73c8
fernando zuniga <carlosfernando.zh@gmail.com>
2022-10-13 22:06:12 -0500

    solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :restaurants, only: [:index, :new, :create, :show]
-  resources :restaurants do
+  #resources :restaurants, only: [:index, :new, :create, :show]
+  resources :restaurants , only: [:index, :new, :create, :show] do
     resources :reviews, only: [:new, :create]
     end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
